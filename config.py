bc0934d0e72a011c471e2b64668072d8e6cb14d8
Дмитрий Дьячков <108222110+dimonstr1980@users.noreply.github.com>
2022-11-28 18:22:57 +0300

    keys were deleted

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = '5939572755:AAH7uVI6j4uJMFtFAfzHXIElmJLOYhGzURg'
+TOKEN = ''
 
-api_key = '9a68a7678f332ea2d00e0521f6a4ba9f9fdd7ea40db3d0ddf6699455d4a8594b'
+api_key = ''
 
 site = 'https://min-api.cryptocompare.com/data/'
 
